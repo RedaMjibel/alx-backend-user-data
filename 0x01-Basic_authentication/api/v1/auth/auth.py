146a03c89e0c35c1c2819877aa9f529efa66317b
Reda Mjibel <reda_mjibel@hotmail.com>
2024-04-17 00:26:17 +0000

    pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,11 +13,9 @@ class Auth:
         """Checks if authentication is required for the given path."""
         return False
 
-
     def authorization_header(self, request=None) -> str:
-         """Extracts the authorization header from the request."""
-         return  None
-
+        """Extracts the authorization header from the request."""
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Gets the current user from the request."""
