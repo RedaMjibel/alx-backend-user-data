2b49959ed8155ed4d84e428cdec5c3039f1788e7
Reda Mjibel <reda_mjibel@hotmail.com>
2024-04-26 01:49:15 +0000

    update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-
-"""DB module
+"""DB module.
 """
 from sqlalchemy import create_engine, tuple_
 from sqlalchemy.exc import InvalidRequestError
@@ -13,20 +12,20 @@ from user import Base, User
 
 
 class DB:
-    """DB class
+    """DB class.
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
+        """Initialize a new DB instance.
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
+        """Memoized session object.
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -34,16 +33,13 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Adds a new user to the database
+        """Adds a new user to the database.
         """
-        if not email or not hashed_password:
-            return
-
         try:
             new_user = User(email=email, hashed_password=hashed_password)
-            session = self._session
-            session.add(new_user)
-            session.commit()
-            return new_user
-        except IntegrityError as e:
-            session.rollback()
+            self._session.add(new_user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            new_user = None
+        return new_user
