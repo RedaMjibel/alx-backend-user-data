0686dc8ffe907f1f2425622bf0d4eacb23f4a042
Reda Mjibel <reda_mjibel@hotmail.com>
2024-04-26 02:48:52 +0000

    auh class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,3 +16,11 @@ def _hash_password(password: str) -> str:
     takes in a password string arguments and returns bytes.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
